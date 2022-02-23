6370fb7139dec886f64b7ae53a7cbaa668e56cb6
ParthD77 <99554534+ParthD77@users.noreply.github.com>
2022-02-23 14:24:04 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,12 +1,25 @@
-user = "Mythic".lower()
-password = "parth"
+import time
+user = ["Mythic"]
+password = ["parth"]
 
-userAtempt = input("Username?\n")
-if userAtempt.lower() == user:
-    userPass = input("Password?\n")
-    if userPass == password:
-        print ("Welcome!")
+while True:
+    broken = False
+    userAtempt = input("Username?\n")
+    if userAtempt in user:
+        while True:
+            userPass = input("Password?\n")
+            if userPass in password:
+                print ("Welcome!")
+                broken = True
+                if broken:
+                 break
+            else:
+                usertry = input("Incorrect\n Would you like to try again. \nType yes or no.?\n")
+                if usertry == ("no"):
+                    broken = True
+                    break
     else:
-        print ("Incorrect")
-else:
-    print ("Incorrect")
+        usertry = input("Incorrect\n Would you like to try again. \nType yes or no.?\n")
+        if usertry == ("no"):
+            broken = True
+            break
