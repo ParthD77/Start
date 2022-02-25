0e3599866c49f5b69f94424b4fda99d3c9951c05
ParthD77 <99554534+ParthD77@users.noreply.github.com>
2022-02-25 11:25:32 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,25 +1,36 @@
 import time
-user = ["Mythic"]
+
+user = ["mythic"]
 password = ["parth"]
 
 while True:
     broken = False
     userAtempt = input("Username?\n")
+    userAtempt = userAtempt.lower()
     if userAtempt in user:
-        while True:
             userPass = input("Password?\n")
             if userPass in password:
                 print ("Welcome!")
                 broken = True
                 if broken:
-                 break
+                    break
             else:
                 usertry = input("Incorrect\n Would you like to try again. \nType yes or no.?\n")
-                if usertry == ("no"):
+                if usertry == ("yes"):
+                    print ("Okay try again")
+                elif usertry == ("no"):
                     broken = True
-                    break
+                    if broken:
+                        break
+                elif usertry != ("yes", "no"):
+                    print ("Please only enter yes or no")
     else:
         usertry = input("Incorrect\n Would you like to try again. \nType yes or no.?\n")
-        if usertry == ("no"):
+        if usertry == ("yes"):
+            print ("Okay try again")
+        elif usertry != ("yes" or "no"):
+            print ("Please only enter yes or no")
             broken = True
-            break
+            if broken:
+                break
+
